60229490e2196f3c95b2918374caff3c84f0a124
morevishal99 <morevishal060298@gmail.com>
2023-01-04 20:52:20 +0530

    Added Navbar and Resume

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-  <h1> Hi I Am Vishal More</h1>
+      <Navbar />
     </div>
   );
 }
