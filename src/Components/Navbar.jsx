385d44f5f126f51925ac6a532adee48de0fd6ae8
morevishal99 <morevishal060298@gmail.com>
2023-06-01 15:20:26 +0530

    made some changes in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import logod from '../Images/VISHAL_DARK.gif'
 import logol from '../Images/VISHAL_LIGHT.gif'
 // import logo from '../Images/Vishal.gif'
 import { useContext } from 'react';
-import { Box, Flex, IconButton, useDisclosure, Stack, Text, Image } from '@chakra-ui/react';
+import { Box, Flex, IconButton, useDisclosure, Stack, Text, Image, Button } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs';
 import { AppContext } from '../Context/Theme';
@@ -21,11 +21,16 @@ export const Navbar = ({ ProfileRef, AboutRef, SkillsRef, ProjectsRef, ContactRe
         backgroundColor: 'black',
         color: 'white'
     }
-
+    const Resume = () => {
+        // window.open(pdf);
+        // window.location.href = "https://drive.google.com/uc?export=download&id=1t42g2b_cVik65BL7Yni89_JzRYihcs94";
+        window.location.href = "https://drive.google.com/uc?export=download&id=1MT0lyDc2sz4cRTEJHsArdDrLWbebLhE9";
+        // window.open("https://drive.google.com/file/d/1t42g2b_cVik65BL7Yni89_JzRYihcs94/view");
+    }
     return (
         <>
-            <Box   id="nav-menu" position='fixed' w='100%' zIndex='100' px={4} style={Theme === 'light' ? light : dark} className='ChackraNavBar' shadow='lg'>
-                   <Flex alignItems={'left'} justifyContent={'space-between'} >
+            <Box id="nav-menu" position='fixed' w='100%' zIndex='100' px={4} style={Theme === 'light' ? light : dark} className='ChackraNavBar' shadow='lg'>
+                <Flex alignItems={'left'} justifyContent={'space-between'} >
                     <IconButton style={Theme === 'light' ? light : dark}
                         size='lg'
                         icon={isOpen ? <CloseIcon /> : <HamburgerIcon fontSize='30px' />}
@@ -37,7 +42,7 @@ export const Navbar = ({ ProfileRef, AboutRef, SkillsRef, ProjectsRef, ContactRe
                     />
                     <Flex justifyContent={'space-between'} alignItems='center' w='90%' m={'auto'} >
                         <Box onClick={ProfileRef} p='5px'>
-                            <Image h={{ base: '60px', md: '60px',lg:"80px" }}width={{ base: '120px', md: '140px',lg:"190px" }} src={ Theme === 'light' ? logol : logod } />
+                            <Image h={{ base: '60px', md: '60px', lg: "80px" }} width={{ base: '120px', md: '140px', lg: "190px" }} src={Theme === 'light' ? logol : logod} />
                         </Box>
                         <Flex as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }} gap='10px' w={{ base: '', md: '60%', lg: '40%' }}
                             justifyContent='space-between' alignItems='center' color='goldenrod' fontWeight='650'>
@@ -46,9 +51,18 @@ export const Navbar = ({ ProfileRef, AboutRef, SkillsRef, ProjectsRef, ContactRe
                             <Text id="skills" className="nav-link skills" onClick={SkillsRef} cursor='pointer'>Skills</Text>
                             <Text id="projects" className="nav-link projects" onClick={ProjectsRef} cursor='pointer'>Project</Text>
                             <Text className="nav-link contact" onClick={ContactRef} cursor='pointer'>Contact</Text>
-                            
-                            <a id="resume-button-1" className="nav-link resume" href="https://drive.google.com/uc?export=download&id=1MT0lyDc2sz4cRTEJHsArdDrLWbebLhE9" target={"_blank"} rel="noreferrer" > Resume </a> 
+
+                            {/* <a id="resume-button-1" className="nav-link resume" href="https://drive.google.com/uc?export=download&id=1MT0lyDc2sz4cRTEJHsArdDrLWbebLhE9" target={"_blank"} rel="noreferrer" > Resume </a>  */}
                             {/* <a id="resume-button-1" className="nav-link resume" href="https://drive.google.com/file/d/1MT0lyDc2sz4cRTEJHsArdDrLWbebLhE9/view?usp=share_link" target={"_blank"} rel="noreferrer" > Resume </a>  */}
+                            <Text id="resume-button-2" onClick={Resume}  fontSize={{ base: 'sm', md: 'md', lg: 'lg' }} mt={{ base: '10px', md: '0' }} colorScheme='teal' gap={1.5}>
+                                {/* <a id="resume-link-2" href={"https://drive.google.com/file/d/1t42g2b_cVik65BL7Yni89_JzRYihcs94/view"} target="_blank" rel="noreferrer"   > */}
+                                <a id="resume-link-2" href={"https://drive.google.com/file/d/1MT0lyDc2sz4cRTEJHsArdDrLWbebLhE9/view?usp=share_link"} target="_blank" rel="noreferrer"   >
+                                    Resume
+                                </a>
+                                {/* <a id="resume-link-2"  href={pdf}  target="_blank" rel="noreferrer"   >
+                                Download Resume
+                            </a> */}
+                            </Text>
                         </Flex>
                     </Flex>
                     <Box width='10%' mt={"20px"} justifyContent={'center'} display='flex'>
@@ -63,7 +77,7 @@ export const Navbar = ({ ProfileRef, AboutRef, SkillsRef, ProjectsRef, ContactRe
                             <Text id="skills" className="nav-link skills" onClick={SkillsRef} cursor='pointer'>Skills</Text>
                             <Text id="projects" className="nav-link projects" onClick={ProjectsRef} cursor='pointer'>Project</Text>
                             <Text id="contact" className="nav-link contact" onClick={ContactRef} cursor='pointer'>Contact</Text>
-                            <a  className="nav-link resume" href="https://drive.google.com/file/d/1MT0lyDc2sz4cRTEJHsArdDrLWbebLhE9/view?usp=share_link" target='_blank' rel="noreferrer">Resume </a>
+                            <a className="nav-link resume" href="https://drive.google.com/file/d/1MT0lyDc2sz4cRTEJHsArdDrLWbebLhE9/view?usp=share_link" target='_blank' rel="noreferrer">Resume </a>
                         </Stack>
                     </Box>
                 ) : null}
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -12,83 +12,89 @@ export default function About() {
     color: 'teal'
   }
   return (
-    <><Box id="about" className="about section">
-      <Text style={{textShadow:"2px 2px red"}}  mt={{ sm: '20%', md: '10%', lg: '15%' }} fontSize={{ base: '22px', md: '22px', lg: '40px' }} color='goldenrod' fontWeight={{ base: '600', md: '700' }} textAlign='center'>About Me</Text>
-      <Flex  justify={'center'} display={{ base: "column", sm: "column", md: "flex", lg: "flex" }} margin="auto" >
-        <Box  id="user-detail-intro" style={Theme === 'light' ? light : dark}
-          maxW={{ base: '350px',sm:"400px", md: '450px', lg: '600px', xl: '800px' }}
-          w={'full'}
-          bg={useColorModeValue('white', 'gray.900')}
-          rounded={'lg'}
-          color="red"
-          textAlign={'center'}>
+    <>
+      <Box id="about" className="about section">
+        <Text style={{ textShadow: "2px 2px red" }} mt={{ sm: '20%', md: '10%', lg: '15%' }} fontSize={{ base: '22px', md: '22px', lg: '40px' }} color='goldenrod' fontWeight={{ base: '600', md: '700' }} textAlign='center'>About Me</Text>
+        <Flex justifyContent={"center"} display={{ base: "column", sm: "column", md: "flex", lg: "flex" }} width="90%" margin="auto" >
+          <Box  id="user-detail-intro" style={Theme === 'light' ? light : dark}
+            maxW={{ base: '350px', sm: "400px", md: '450px', lg: '600px', xl: '800px' }}
+            w={'full'}
+            bg={useColorModeValue('white', 'gray.900')}
+            rounded={'lg'}
+            color="red"
+            textAlign={'center'}
+            margin="auto">
 
-          <Text fontSize={"20px"} fontWeight="600" mt={"10px"} textAlign={{ base: "center", sm: "center", md: "left", lg: "left" }} px={8} style={Theme === 'light' ? light : dark} color="red">
-            An aspiring full-stack web developer with a good knowledge of web development and Problem-Solving skills. Skilled in developing quality
-            programs quickly. My expertise lies in delivering user-friendly software
-            applications within strict deadlines. Looking to secure a stimulating
-            position as a Full stack web developer, alongside learning and growing
-            with the company.
-          </Text>
-          
-          <Stack align={'center'} justify={'center'} direction={{ base: 'column', md: "row", lg: "row" }} mt={6}>
-            <Text
-              px={2}
-              py={1}
-              bg={useColorModeValue('gray.50', 'gray.800')}
-              fontWeight={'400'}>
-              #HTML
+            <Text fontSize={"20px"} fontWeight="600" mt={"10px"} textAlign={{ base: "center", sm: "center", md: "center", lg: "left" }} px={8} style={Theme === 'light' ? light : dark} color="red">
+              An aspiring full-stack web developer with a good knowledge of web development and Problem-Solving skills. Skilled in developing quality
+              programs quickly. My expertise lies in delivering user-friendly software
+              applications within strict deadlines. Looking to secure a stimulating
+              position as a Full stack web developer, alongside learning and growing
+              with the company.
             </Text>
 
-            <Text
-              px={2}
-              py={1}
-              bg={useColorModeValue('gray.50', 'gray.800')}
-              fontWeight={'400'}>
-              #CSS
-            </Text>
-            <Text
-              px={2}
-              py={1}
-              bg={useColorModeValue('gray.50', 'gray.800')}
-              fontWeight={'400'}>
-              #Javascript
-            </Text>
-            <Text
-              px={2}
-              py={1}
-              bg={useColorModeValue('gray.50', 'gray.800')}
-              fontWeight={'400'}>
-              #REACT JS
-            </Text>
-            <Text
-              px={2}
-              py={1}
-              bg={useColorModeValue('gray.50', 'gray.800')}
-              fontWeight={'400'}>
-              #NODE JS
-            </Text>
-            <Text
-              px={2}
-              py={1}
-              bg={useColorModeValue('gray.50', 'gray.800')}
-              fontWeight={'400'}>
-              #EXPRESS JS
-            </Text>
-            <Text
-              px={2}
-              py={1}
-              bg={useColorModeValue('gray.50', 'gray.800')}
-              fontWeight={'400'}>
-              #MONGO DB
-            </Text>
+            <Stack align={'center'}  direction={{ base: 'column',sm:"column", md: "row", lg: "row" }} mt={6}>
+              <Text
+                px={2}
+                py={1}
+                bg={useColorModeValue('gray.50', 'gray.800')}
+                fontWeight={'400'}>
+                #HTML
+              </Text>
+
+              <Text
+                px={2}
+                py={1}
+                bg={useColorModeValue('gray.50', 'gray.800')}
+                fontWeight={'400'}>
+                #CSS
+              </Text>
+              <Text
+                px={2}
+                py={1}
+                bg={useColorModeValue('gray.50', 'gray.800')}
+                fontWeight={'400'}>
+                #Javascript
+              </Text>
+              <Text
+                px={2}
+                py={1}
+                bg={useColorModeValue('gray.50', 'gray.800')}
+                fontWeight={'400'}>
+                #REACT JS
+              </Text>
+              <Text
+                px={2}
+                py={1}
+                bg={useColorModeValue('gray.50', 'gray.800')}
+                fontWeight={'400'}>
+                #NODE JS
+              </Text>
+              <Text
+                px={2}
+                py={1}
+                bg={useColorModeValue('gray.50', 'gray.800')}
+                fontWeight={'400'}>
+                #EXPRESS JS
+              </Text>
+              <Text
+                px={2}
+                py={1}
+                bg={useColorModeValue('gray.50', 'gray.800')}
+                fontWeight={'400'}>
+                #MONGO DB
+              </Text>
 
 
-          </Stack>
-        </Box>
-        <Image src="https://camo.githubusercontent.com/101c98f67298425e9875ecfac1c25efccaf3c966db04609cc4f4e92d6bd12c32/68747470733a2f2f632e74656e6f722e636f6d2f4e4f594633663832625f6741414141432f70726f6772616d6d65722e676966" alt="coding image" />
-      </Flex>
-    </Box>
+            </Stack>
+          </Box>
+
+          <Box  >
+
+          <Image  margin={"auto"}src="https://camo.githubusercontent.com/101c98f67298425e9875ecfac1c25efccaf3c966db04609cc4f4e92d6bd12c32/68747470733a2f2f632e74656e6f722e636f6d2f4e4f594633663832625f6741414141432f70726f6772616d6d65722e676966" alt="coding image" />
+          </Box>
+        </Flex>
+      </Box>
 
     </>
   );
